aeb38784cb368ca96bb2c3e804519d665c00bf3f
l1salvatore <lucas_salva_15@hotmail.com>
2025-07-31 08:37:00 -0300

    Test

--- TestStateMachine/Program.cs
+++ TestStateMachine/Program.cs
@@ -1,6 +1,6 @@
 ﻿using StateMachine;
 
-ExampleStateMachine stateMachine = new ExampleStateMachine();
+ExampleStateMachine stateMachine = new ExampleStateMachine(); // 
 var s = stateMachine.CurrentState;
 bool b = stateMachine.CanTransition(ExampleState.CategoryTwo);
 stateMachine.ExecuteTransition(ExampleState.CategoryTwo);
