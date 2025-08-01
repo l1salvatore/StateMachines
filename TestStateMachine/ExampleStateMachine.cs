e1811fcdf9ca697f9eb50461f2b63fdfcfa5f7cc
l1salvatore <lucas_salva_15@hotmail.com>
2025-08-01 18:47:03 -0300

    +ExampleStateMachine

---
+++ TestStateMachine/ExampleStateMachine.cs
@@ -0,0 +1,35 @@
+﻿using StateMachine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestStateMachine
+{
+
+    public enum ExampleState
+    {
+        CategoryOne,
+        CategoryTwo,
+        CategoryThree,
+        Quit
+    }
+
+    public class ExampleStateMachine : StateMachine<ExampleState>
+    {
+        public ExampleStateMachine() : base(ExampleState.CategoryOne, [
+            (ExampleState.CategoryOne, [ExampleState.CategoryOne, ExampleState.CategoryTwo, ExampleState.Quit]),
+        (ExampleState.CategoryTwo, [ExampleState.CategoryTwo, ExampleState.CategoryThree, ExampleState.Quit]),
+        (ExampleState.CategoryThree, [ExampleState.CategoryThree, ExampleState.Quit]),
+        (ExampleState.Quit, [ExampleState.Quit])
+
+            ])
+        { }
+
+        public override bool OnIdleState()
+        {
+            return false;
+        }
+    }
+}
--- TestStateMachine/Program.cs
+++ TestStateMachine/Program.cs
@@ -7,30 +7,6 @@ stateMachine.ExecuteTransition(ExampleState.CategoryTwo);
 b = stateMachine.CanTransition(ExampleState.CategoryOne);
 ;
 
-public class ExampleStateMachine : StateMachine<ExampleState>
-{
-    public ExampleStateMachine() : base(ExampleState.CategoryOne, [
-        (ExampleState.CategoryOne, [ExampleState.CategoryOne, ExampleState.CategoryTwo, ExampleState.Quit]),
-        (ExampleState.CategoryTwo, [ExampleState.CategoryTwo, ExampleState.CategoryThree, ExampleState.Quit]),
-        (ExampleState.CategoryThree, [ExampleState.CategoryThree, ExampleState.Quit]),
-        (ExampleState.Quit, [ExampleState.Quit])
-
-        ])
-    { }
-
-    public override bool OnIdleState()
-    {
-        return false;
-    }
-}
-
-public enum ExampleState
-{ 
-    CategoryOne,
-    CategoryTwo,
-    CategoryThree,
-    Quit
-}
 
 
 
